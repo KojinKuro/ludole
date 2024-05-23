b57a77fbdc5186a7c6d7605b810977e370cc1baa
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-05-23 09:39:06 -0600

    feat: Will add a container to hide overflow and sharpen edges of blurred image.

--- src/ImageRender/ImageRender.jsx
+++ src/ImageRender/ImageRender.jsx
@@ -1,9 +1,13 @@
 function ImageRender({gameImage, guessNumber }){
      //if you want to see the css trigger when working on integrating this with props uncomment below.
-     // guessNumber = 0 
-    return (<img className="game-img" id={`guess-${guessNumber}`}src={`${gameImage}`}></img>)
-}
-export default ImageRender
+    //  guessNumber = 0 
+    return (
+    <div className="blur-overflow">
+        <img alt='game-image' className="game-img" id={`guess-${guessNumber}`}src={`${gameImage}`}></img>
+    </div>
+    );
+};
+export default ImageRender;
 
 
 //https://www.kalmbachfeeds.com/cdn/shop/articles/two-white-ducks-in-grass.jpg?v=1706873608 sample image to plug into src if you would like to see it working,
