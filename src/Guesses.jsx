ce5c0842753182c24c492f96887223aba6a237aa
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-05-22 19:45:18 -0600

    add basic state to component

--- src/Guesses.jsx
+++ src/Guesses.jsx
@@ -2,5 +2,6 @@ import { useState, useEffect } from 'react';
 import "./Guesses.css"
 
 export default function Guesses({ guessArray }) {
-    
+    const [boxes, setBoxes] = useState(Array(8).fill(''));
+    const [currentIndex, setCurrentIndex] = useState(0);
   }
