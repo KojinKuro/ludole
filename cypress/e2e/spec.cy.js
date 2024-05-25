f57b56a5b4885398ad04d980843efd9c86ee97c3
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-05-24 18:18:56 -0600

    feat: Will add a test checking for an incorrect guess.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,10 +6,10 @@ describe('basic function check', () => {
   })
 })
 
-describe('Landing page game sad paths.', () => {
+describe('Landing page game.', () => {
   beforeEach(()=>{
     cy.visit('http://127.0.0.1:5173/')
-  })
+  });
   it('As a user if I make a guess that is not currently in the possible titles, I will receive a message that my guess is invalid.', () => {
     cy.get('input')
     .type('hello')
@@ -18,4 +18,13 @@ describe('Landing page game sad paths.', () => {
     cy.get('#game > :nth-child(3)')
     .should('contain', 'Could not find your game')
   })
-})
+  it('As a user if guess incorrectly the attempts counter should tick up, and my guess should populate in red in the attempts.',()=>{
+    cy.get('input')
+    .type("Super Mario World 2: Yoshi's Island")
+    cy.get('button')
+    .click()
+    cy.get('.guess-container > :nth-child(1)')
+    .invoke('attr','class')
+    .should('contain', 'red')
+  })
+});
