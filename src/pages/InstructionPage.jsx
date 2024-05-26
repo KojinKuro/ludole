c329a039eddded77244d7d5e55a2958fa2af6326
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-05-26 13:16:15 -0600

    add css styling to instruction page

--- src/pages/InstructionPage.jsx
+++ src/pages/InstructionPage.jsx
@@ -1,8 +1,10 @@
+import "./InstructionPage.css"
+
 export default function InstructionPage() {
   return (
-    <main>
+    <main className="instructions">
       <h1> How to Play</h1>
-      <div>Guess the video game in 9 tries</div>
+      <div>Guess the video game in 8 tries</div>
       <ul>
         <li>Each guess must be a valid game from our database.</li>
         <li>
