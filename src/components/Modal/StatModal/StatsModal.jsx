f0244d4e5f1f93971242921e9946549cd908f972
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-06-25 04:05:52 -0700

    Add stat bars

--- src/components/Modal/StatModal/StatsModal.jsx
+++ src/components/Modal/StatModal/StatsModal.jsx
@@ -1,5 +1,7 @@
 import {
+  Box,
   Button,
+  Flex,
   IconButton,
   Modal,
   ModalBody,
@@ -8,12 +10,72 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
-  Text,
   useDisclosure,
 } from "@chakra-ui/react";
 
+import {
+  BarController,
+  BarElement,
+  CategoryScale,
+  Chart,
+  LinearScale,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+Chart.register(CategoryScale, BarController, BarElement, LinearScale);
+
+import { loadGame } from "../../../javascript/save";
+
 export default function StatsModal() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const saveData = loadGame();
+  const gameStats = Object.keys(saveData || {}).reduce(
+    (gameStats, date) => {
+      const { hasWon, guessHistory, totalGuesses } = saveData[date];
+      const guessCount = guessHistory.length;
+      const gameCompleted = Boolean(hasWon || guessCount === totalGuesses);
+
+      while (guessCount > gameStats.guessData.length) {
+        gameStats.guessData.push(0);
+      }
+
+      if (gameCompleted) gameStats.guessData[guessCount - 1] += 1;
+
+      return {
+        gamesPlayed: gameCompleted
+          ? gameStats.gamesPlayed + 1
+          : gameStats.gamesPlayed,
+        gamesWon: hasWon ? gameStats.gamesWon + 1 : gameStats.gamesWon,
+        guessData: gameStats.guessData,
+        maxTotalGuesses:
+          totalGuesses > gameStats.maxTotalGuesses
+            ? totalGuesses
+            : gameStats.maxTotalGuesses,
+      };
+    },
+    {
+      gamesPlayed: 0,
+      gamesWon: 0,
+      guessData: [],
+      maxTotalGuesses: 0,
+    }
+  );
+
+  while (gameStats.guessData.length < gameStats.maxTotalGuesses) {
+    gameStats.guessData.push(0);
+  }
+
+  const myData = {
+    labels: Object.keys(gameStats.guessData).map((val) => {
+      return parseInt(val) + 1;
+    }),
+    datasets: [
+      {
+        label: "Guess Distribution",
+        data: gameStats.guessData,
+      },
+    ],
+  };
 
   return (
     <>
@@ -29,7 +91,18 @@ export default function StatsModal() {
           <ModalHeader>Stats</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Text>Stats page</Text>
+            <Flex direction="column">
+              <Box>Games Played{gameStats.gamesPlayed}</Box>
+              <Box>
+                Win percentage
+                {Math.trunc((gameStats.gamesWon / gameStats.gamesPlayed) * 100)}
+                %
+              </Box>
+              <Bar
+                options={{ indexAxis: "y", responsive: true }}
+                data={myData}
+              />
+            </Flex>
           </ModalBody>
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={onClose}>
