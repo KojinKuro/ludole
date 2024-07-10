11763f25a1a581f795b3157e565067139b633834
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-07-10 16:39:51 -0700

    Add links to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
   return (
     <footer className="main-footer">
-      Coded by Brandon Doza, Charles Kwang, Gwyneth Patrick, Lydia Sims
+      Coded by <Link to="https://github.com/BrandonDoza">Brandon Doza</Link>,{" "}
+      <Link to="https://github.com/KojinKuro">Charles Kwang</Link>,{" "}
+      <Link to="https://github.com/moth-dust">Gwyneth Patrick</Link>,{" "}
+      <Link to="https://github.com/LISims88">Lydia Sims</Link>
     </footer>
   );
 }
